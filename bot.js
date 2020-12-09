b53528fd2bfd9689cec14b900f6056e048ce06d3
Robby Horvath <horvathrobby@gmail.com>
2020-12-09 15:07:52 -0500

    Add more commands and Firebase Support

--- bot.js
+++ bot.js
@@ -1,61 +1,109 @@
-const config = require('./config.json')
+const config = require('./config.json') // config file to store API keys
 const command = require('./command')
 
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-const firebase = require('firebase/app')
-const firebaseDB = firebase.initializeApp({
+const firebase = require('firebase')
+firebase.initializeApp({
     apiKey: config.firebase_key,
     databaseURL: config.firebase_url,
     projectId: config.firebase_project_id
 })
+const database = firebase.database()
 
-const Spotify = require('spotify-web-api-node')
-const spotifyApi = new Spotify({
+const SpotifyWebApi = require('spotify-web-api-node')
+const spotifyApi = new SpotifyWebApi({
     clientId: config.spotify_client_id,
     clientSecret: config.spotify_client_secret,
-    accessToken: config.spotify_access_token
-});
-
-const jpegMafiaID = '6yJ6QQ3Y5l0s0tn7b0arrO'
-
-client.on('ready', () => {
-    console.log("Discord Client Ready")
-
-    command(client, 'add', message => {
-        try {
-            console.log(message.content)
-            spotifyApi.searchArtists(message.content.replace(`${config.prefix}add `, '')).then(
-                data => {
-                    client.channels.cache.get('784183490543222784').send('Artist albums', JSON.stringify(data.body.items));
-                    console.log(data.body.artists.items[0])
-                }, error => {
-                    console.log(error)
-                }
-            )
-
-        } finally {
-            message.channel.send('Adding artist')
-        }
-    })
-
-    command(client, ['cc', 'clearchannel'], message => {
-        if (message.member.hasPermission('ADMINISTRATOR')) {
-            message.channel.messages.fetch().then((results) => {
-                message.channel.bulkDelete(results)
-            })
-        }
-    })
-
-    spotifyApi.getArtistAlbums(jpegMafiaID).then(
-        data => {
-            client.channels.cache.get('784183490543222784').send('Artist albums', JSON.stringify(data.body.items[0].name));
-        }, error => {
-            console.error(error);
-        }
-    );
-
 })
 
+let artistArray = new Array()
+
+async function main() {
+    await fillArtistArray()
+    await refreshAccessToken()
+
+    client.on('ready', () => {
+        console.log("Discord Client Ready")
+
+        command(client, 'add', message => {
+            let artistName = message.content.replace(`${config.prefix}add `, '')
+            if (message.content === `${config.prefix}add` || !artistName) {
+                message.channel.send(`Enter a valid artist name. i.e. \'${config.prefix}add Drake\'`)
+            } else {
+                spotifyApi.searchArtists(artistName, {
+                    limit: 1
+                }).then(
+                    data => {
+                        let artistData = data.body.artists.items[0]
+                        if (artistData) {
+                            message.channel.send(`${artistName}:  ${artistData.name}`)
+                            addNewArtist(artistData.name.replace('$', 'S').replace(',', ''), artistData.uri.replace('spotify:artist:', ''))
+                        } else {
+                            message.channel.send(`Enter a valid artist name. i.e. \'${config.prefix}add Drake\'`)
+                        }
+                    }, err => {
+                        console.error(err)
+                    }
+                )
+            }
+        })
+
+        command(client, ['ls', 'list'], message => {
+            if (artistArray) {
+                let output = '**__All artists__**: \n\n'
+                artistArray.forEach(artist => {
+                    output += `${artist.name} ${artist.uri}\n`
+                })
+                message.channel.send(output)
+            }
+        })
+
+        command(client, ['cc', 'clearchannel'], message => {
+            if (message.member.hasPermission('ADMINISTRATOR')) {
+                message.channel.messages.fetch().then((results) => {
+                    message.channel.bulkDelete(results)
+                })
+            }
+        })
+
+    })
+}
+
+main()
 client.login(config.discord_token)
+
+async function addNewArtist(name, uri) {
+    database.ref('artists/').child(name).set(uri);
+}
+
+async function fillArtistArray() {
+    database.ref('artists/').once('value', snapshot => {
+        let counter = 0
+        snapshot.forEach(childSnapshot => {
+            artistArray.push({
+                name: Object.keys(snapshot.val())[counter],
+                uri: childSnapshot.val()
+            })
+            counter++
+        })
+    }, err => {
+        console.error(err)
+    })
+}
+
+async function refreshAccessToken() {
+    spotifyApi.clientCredentialsGrant().then(
+        data => {
+            // console.log(data.body)
+            // console.log('The access token expires in ' + data.body['expires_in']);
+            // console.log('The access token is ' + data.body['access_token']);
+
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+        }, err => {
+            console.log('Something went wrong when retrieving an access token', err);
+        }
+    )
+}
